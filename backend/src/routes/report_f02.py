4331d744a924f86c847a69b34b9cd8fba306951f
sila1404 <silamany1404@gmail.com>
2024-10-16 20:20:34 +0700

    fix: make the method whjch calculate the total to work

--- backend/src/routes/report_f02.py
+++ backend/src/routes/report_f02.py
@@ -1,24 +1,18 @@
 from fastapi import APIRouter, Request
+import pandas as pd
 from services import F02
 
-f02_route = APIRouter(prefix="/reports/f02")
-
-report_f02 = None
+f02_route = APIRouter(prefix="/reports")
 
 
-@f02_route.get("/1")
+@f02_route.get("/f02")
 async def get_first_f02_total(req: Request):
-    report: dict[str, any] = req.app.state.report
-    if report is None:
+    df_report: pd.DataFrame = req.app.state.df_report
+    if df_report is None:
         return {"error": "No report available. Please upload the file first"}
 
-    f02 = F02(report)
+    f02 = F02(df_report)
 
-    return f02.file
+    return {"first_report": f02.first_report()}
 
 
-# @f02_route.get("/test")
-# def test(req: Request):
-#     state = req.app.state.report
-#     print(state)
-#     return "Hello world"
