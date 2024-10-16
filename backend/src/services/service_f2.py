eb7f1d715b716d0f5e510680d3ba7e5e7d32b324
sila1404 <silamany1404@gmail.com>
2024-10-16 20:25:12 +0700

    refactor: rename the file to be more specific

--- backend/src/services/service_f2.py
+++ backend/src/services/service_f2.py
@@ -1,14 +1,14 @@
 import pandas as pd
-from utils import get_f02_closing_balance
+from utils import get_f2_closing_balance
 
 
-class F02:
+class F2:
     def __init__(self, file: pd.DataFrame) -> None:
         self.file = file
 
     def first_report(self) -> dict[str, int]:
-        report_1 = get_f02_closing_balance(self.file, 1101)
-        report_3 = get_f02_closing_balance(self.file, 112117)
+        report_1 = get_f2_closing_balance(self.file, 1101)
+        report_3 = get_f2_closing_balance(self.file, 112117)
         total = report_1 + report_3
         return {
             "total": total,
