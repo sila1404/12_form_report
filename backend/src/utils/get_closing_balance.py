64c8623e3712d56f483596346446a0ed11ef504a
sila1404 <silamany1404@gmail.com>
2024-10-17 09:10:32 +0700

    feat: function to get the closing balance credit base on account code

--- backend/src/utils/get_closing_balance.py
+++ backend/src/utils/get_closing_balance.py
@@ -3,3 +3,6 @@ import pandas as pd
 
 def get_closing_balance_debit(file: pd.DataFrame, ac_code: int) -> int:
     return file[file["Account Code"] == ac_code]["Closing Balance Debit"].iloc[0]
+
+def get_closing_balance_credit(file: pd.DataFrame, ac_code: int) -> int:
+    return file[file["Account Code"] == ac_code]["Closing Balance Credit"].iloc[0]
