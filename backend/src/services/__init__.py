e4128377e00d0bf5d8ccb14de0dd55b673eb89b2
sila1404 <silamany1404@gmail.com>
2024-10-17 10:06:28 +0700

    feat: f3 service to calculate the report

--- backend/src/services/__init__.py
+++ backend/src/services/__init__.py
@@ -1,5 +1,4 @@
 from .f2_service import F2
+from .f3_service import F3
 
-__all__ = [
-    "F2"
-]
+__all__ = ["F2", "F3"]
---
+++ backend/src/services/f3_service.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from utils import get_closing_balance_credit
+
+
+class F3:
+    def __init__(self, file: pd.DataFrame) -> None:
+        self.file = file
+
+    def first_report(self):
+        report_1 = get_closing_balance_credit(self.file, 51013)
+        report_2 = (
+            get_closing_balance_credit(self.file, 51021)
+            + get_closing_balance_credit(self.file, 51028)
+            + get_closing_balance_credit(self.file, 51022)
+        )
+        total = report_1 + report_2
+
+        return {"total": total, "report_1": report_1, "report_2": report_2}
