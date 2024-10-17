f33779a2ef4ba3e8c8309cfe445b0b744cc77e99
sila1404 <silamany1404@gmail.com>
2024-10-17 21:16:48 +0700

    feat: initial scaffold for f2 report

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableRow,
+  TableHeader,
+  TableHead,
+} from "./components/ui/table";
+import ReportHeader from "./components/ReportHeader";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <main className="flex justify-center pt-7">
+      <div className="grid place-items-center w-8/12 p-8 shadow-lg border-2 border-neutral-800">
+        <ReportHeader reportMessage="ຂໍ້ມູນຖານະການເງິນ"/>
+        <Table className="border border-neutral-800 shadow-xl">
+          <TableHeader>
+            <TableRow className="divide-x border border-neutral-800 divide-y divide-neutral-800">
+              <TableHead className="text-center font-bold">ລາຍການ</TableHead>
+              <TableHead className="text-center font-bold">ຈໍານວນເງິນ (ກີບ)</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            <TableRow className="bg-slate-300 border border-neutral-800 divide-x divide-neutral-800 font-bold">
+              <TableCell>1. ເງິນສົດ ແລະ ເງິນຝາກຢູ່ທະນາຄານກາງ</TableCell>
+              <TableCell className="text-right">Total</TableCell>
+            </TableRow>
+            <TableRow className="divide-x divide-neutral-800">
+              <TableCell>1.1 ເງິນສົດ ແລະ ທີ່ຖືວ່າຄືເງິນສົດ</TableCell>
+              <TableCell className="text-right">1.132.541.000</TableCell>
+            </TableRow>
+            <TableRow className="divide-x divide-neutral-800">
+              <TableCell>1.2 ເງິນຝາກບໍ່ມີກຳນົດ</TableCell>
+              <TableCell className="text-right">2.324.000</TableCell>
+            </TableRow>
+            <TableRow className="divide-x divide-neutral-800">
+              <TableCell>1.3 ເງິນຝາກມີກຳນົດ</TableCell>
+              <TableCell className="text-right">12.232.458.000</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    </main>
+  );
 }
 
-export default App
+export default App;
