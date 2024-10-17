e2895cb7caf298854f9094af021eb4afb75ea910
sila1404 <silamany1404@gmail.com>
2024-10-17 09:12:15 +0700

    chore: forget to import function to init file

--- backend/src/utils/__init__.py
+++ backend/src/utils/__init__.py
@@ -1,4 +1,4 @@
 from .read_file import read_file
-from .get_closing_balance import get_closing_balance_debit
+from .get_closing_balance import get_closing_balance_debit, get_closing_balance_credit
 
-__all__ = ["read_file", "get_closing_balance_debit"]
+__all__ = ["read_file", "get_closing_balance_debit", "get_closing_balance_credit"]
