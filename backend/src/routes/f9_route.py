59e446d4b4cad1ffce5aa2f688ad95b1f476da53
sila1404 <silamany1404@gmail.com>
2024-10-21 00:28:57 +0700

    feat: implement route for report f9

---
+++ backend/src/routes/f9_route.py
@@ -0,0 +1,19 @@
+from fastapi import APIRouter, Request, status
+from fastapi.responses import JSONResponse
+import pandas as pd
+
+
+f9_route = APIRouter(prefix="/reports")
+
+
+@f9_route.get("/f9")
+def get_f3_report(req: Request):
+    df_report: pd.DataFrame = req.app.state.df_report
+
+    if df_report is None:
+        return JSONResponse(
+            content={"error": "No report available. Please upload the file first."},
+            status_code=status.HTTP_404_NOT_FOUND,
+        )
+
+    return JSONResponse(content=df_report.to_dict(orient="records"))
