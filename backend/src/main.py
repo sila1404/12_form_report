495342b9f1697b46137a71dd183470a89c62d59b
sila1404 <silamany1404@gmail.com>
2024-10-20 16:46:44 +0700

    feat: allow origin from frontend

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,11 +1,22 @@
 from fastapi import FastAPI, File, UploadFile, status
 from fastapi.responses import JSONResponse
+from fastapi.middleware.cors import CORSMiddleware
 from io import BytesIO
 
 from utils import read_file
 from routes import f2_route, f3_route
 
 app = FastAPI()
+
+origins = ["http://localhost:5173"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.state.report = None
 
