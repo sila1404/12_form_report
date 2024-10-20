8665c583c96e68dffd9b83ff567873100ff129e4
sila1404 <silamany1404@gmail.com>
2024-10-21 00:30:04 +0700

    refactor: using snake_case for the column header to be use with f9 report

--- backend/src/utils/read_file.py
+++ backend/src/utils/read_file.py
@@ -9,8 +9,18 @@ def read_file(file_path: BytesIO | None) -> pd.DataFrame:
             # Format the file to use necessary header
             df.columns = df.iloc[0]
             df = df.drop(df.index[0]).reset_index(drop=True)
+            # Change column header to snake_case
+            df = df.rename(columns={
+                "Account Code": "Account_Code",
+                "Balance Forward Debit": "Balance_Forward_Debit",
+                "Balance Forward Credit": "Balance_Forward_Credit",
+                "Movement in the month Debit": "Movement_in_the_month_Debit",
+                "Movement in the month Credit": "Movement_in_the_month_Credit",
+                "Closing Balance Debit": "Closing_Balance_Debit",
+                "Closing Balance Credit": "Closing_Balance_Credit"
+            })
             # Convert "Account Code" from type object to int
-            df["Account Code"] = df["Account Code"].astype(int)
+            df["Account_Code"] = df["Account_Code"].astype(int)
             return df
         else:
             print("No file path provided")
