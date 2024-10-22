ff6f7f778b6eee0f3f16f04c29f62e2961b315e8
sila1404 <silamany1404@gmail.com>
2024-10-22 19:43:14 +0700

    feat: fetch the data and display it on the page

--- frontend/src/routes/f2.tsx
+++ frontend/src/routes/f2.tsx
@@ -1,3 +1,7 @@
+import { useEffect, useState } from "react";
+import { createFileRoute } from "@tanstack/react-router";
+import ReportHeader from "@/components/ReportHeader";
+import DownloadButton from "@/components/DownloadButton";
 import {
   Table,
   TableBody,
@@ -6,82 +10,92 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { createFileRoute } from "@tanstack/react-router"
-import ReportHeader from "@/components/ReportHeader";
 
-export const Route = createFileRoute('/f2')({
-  component: ReportF2
-})
+import { ReportResponseType } from "./f9";
+
+import { getReportData } from "@/lib/getReportData";
+import { formatNumber } from "@/lib/formatNumberWithComma";
+
+export const Route = createFileRoute("/f2")({
+  component: ReportF2,
+});
 
 function ReportF2() {
+  const [reportData, setReportData] = useState<ReportResponseType>();
+  const [errMsg, setErrMsg] = useState("");
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await getReportData("/f2");
+      if (result.status === 200) {
+        setReportData(result.data);
+        console.log(result.data);
+      } else {
+        setErrMsg(result.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  
+
+  if (!reportData) {
+    return <div>{errMsg}</div>;
+  }
+
+  function flattenData(data: ReportResponseType) {
+    const result: any[] = [];
+    data?.data.forEach((detail) => {
+      result.push({
+        Item: detail[0],
+        "Amount (KIP)": detail[1],
+      });
+    });
+
+    return result;
+  }
+
   return (
     <main className="flex justify-center pt-7">
-      <div className="grid place-items-center w-8/12 p-8 shadow-lg border-2 border-neutral-800">
+      <div className="grid place-items-center w-9/12 p-8 shadow-lg border-2 border-neutral-800">
         <ReportHeader reportMessage="ຂໍ້ມູນຖານະການເງິນ" />
         <Table className="border border-neutral-800 shadow-xl">
           <TableHeader>
             <TableRow className="divide-x border border-neutral-800 divide-y divide-neutral-800">
-              <TableHead className="text-center font-bold">ລາຍການ</TableHead>
-              <TableHead className="text-center font-bold">
-                ຈໍານວນເງິນ (ກີບ)
-              </TableHead>
+              {reportData?.columns.map((colmunsName, index) => (
+                <TableHead key={index} className="text-center font-bold">
+                  {colmunsName}
+                </TableHead>
+              ))}
             </TableRow>
           </TableHeader>
-          <TableBody className="divide-y divide-neutral-800">
-            {/* start report 1 */}
-            <TableRow className="bg-slate-300 border border-neutral-800 divide-x divide-neutral-800 font-bold">
-              <TableCell>1. ເງິນສົດ ແລະ ເງິນຝາກຢູ່ທະນາຄານກາງ</TableCell>
-              <TableCell className="text-right">Total</TableCell>
-            </TableRow>
-            <TableRow className="divide-x divide-neutral-800">
-              <TableCell>1.1 ເງິນສົດ ແລະ ທີ່ຖືວ່າຄືເງິນສົດ</TableCell>
-              <TableCell className="text-right">1.132.541.000</TableCell>
-            </TableRow>
-            <TableRow className="divide-x divide-neutral-800">
-              <TableCell>1.2 ເງິນຝາກບໍ່ມີກຳນົດ</TableCell>
-              <TableCell className="text-right">2.324.000</TableCell>
-            </TableRow>
-            <TableRow className="divide-x divide-neutral-800">
-              <TableCell>1.3 ເງິນຝາກມີກຳນົດ</TableCell>
-              <TableCell className="text-right">12.232.458.000</TableCell>
-            </TableRow>
-            {/* end report 1 */}
-            {/* start report 2 */}
-            <TableRow className="bg-slate-300 divide-x divide-neutral-800 font-bold">
-              <TableCell>2. ໜີ້ຕ້ອງຮັບຈາກສະຖາບັນການເງິນອື່ນ</TableCell>
-              <TableCell className="text-right">Total</TableCell>
-            </TableRow>
-            <TableRow className="divide-x divide-neutral-800">
-              <TableCell>2.1 ເງິນຝາກບໍ່ມີກຳນົດ</TableCell>
-              <TableCell className="text-right">1.132.541.000</TableCell>
-            </TableRow>
-            <TableRow className="divide-x divide-neutral-800">
-              <TableCell>2.2 ເງິນຝາກມີກຳນົດ</TableCell>
-              <TableCell className="text-right">2.324.000</TableCell>
-            </TableRow>
-            <TableRow className="divide-x divide-neutral-800">
-              <TableCell>2.3 ເງິນໃຫ້ກູ້ຢືມ ແລະ ເງິນລ່ວງໜ້າສຸດທິ</TableCell>
-              <TableCell className="text-right">12.232.458.000</TableCell>
-            </TableRow>
-            {/* end report 2 */}
-            {/* start report 3 */}
-            <TableRow className="bg-slate-300 divide-x divide-neutral-800 font-bold">
-              <TableCell>3. ຫຼັກຊັບຊື້ໂດຍມີສັນຍາຂາຍຄືນ</TableCell>
-              <TableCell className="text-right">Total</TableCell>
-            </TableRow>
-            <TableRow className="divide-x divide-neutral-800">
-              <TableCell>3.1 ຫຼັກຊັບຊື້ໂດຍມີສັນຍາຂາຍຄືນ</TableCell>
-              <TableCell className="text-right">1.132.541.000</TableCell>
-            </TableRow>
-            <TableRow className="divide-x divide-neutral-800">
-              <TableCell>3.2 ໜີ້ຕ້ອງຮັບທວງຍາກ ແລະ ລາຍການອື່ນໆ</TableCell>
-              <TableCell className="text-right">2.324.000</TableCell>
-            </TableRow>
-            {/* end report 3 */}
+          <TableBody>
+            {reportData?.data.map((data, index) => (
+              <TableRow
+                key={index}
+                className="border border-neutral-800 divide-x divide-neutral-800"
+              >
+                {data.map((detail, index) => (
+                  <TableCell
+                    key={index}
+                    className={
+                      typeof detail === "number" ? "text-right" : "text-left"
+                    }
+                  >
+                    {typeof detail === "number" ? formatNumber(detail) : detail}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))}
           </TableBody>
         </Table>
+        <DownloadButton
+          data={flattenData(reportData)}
+          fileName="Report_F2"
+          sheetName="Report F2"
+        />
       </div>
     </main>
   );
 }
-
