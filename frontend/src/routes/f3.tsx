5f67daa530ae969e6395ffbd449669a73101bf65
sila1404 <silamany1404@gmail.com>
2024-10-23 16:19:32 +0700

    refactor: just write the component within the tanstack router instead of create a function for it

--- frontend/src/routes/f3.tsx
+++ frontend/src/routes/f3.tsx
@@ -1,5 +1,12 @@
 import ReportHeader from "@/components/ReportHeader";
-import { Table, TableBody, TableCell, TableHead, TableHeader, TableRow } from "@/components/ui/table";
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
 import { createFileRoute } from "@tanstack/react-router";
 import { ReportResponseType } from "./f9";
 import { useEffect, useState } from "react";
@@ -8,80 +15,81 @@ import { formatNumber } from "@/lib/formatNumberWithComma";
 import DownloadButton from "@/components/DownloadButton";
 
 export const Route = createFileRoute("/f3")({
-  component: ReportF3,
-});
+  component: () => {
+    const [reportData, setReportData] = useState<ReportResponseType>();
+    const [errMsg, setErrMsg] = useState("");
 
-function ReportF3() {
-  const [reportData, setReportData] = useState<ReportResponseType>();
-  const [errMsg, setErrMsg] = useState("");
+    useEffect(() => {
+      const fetchData = async () => {
+        const result = await getReportData("/f3");
+        if (result.status === 200) {
+          setReportData(result.data);
+        } else {
+          setErrMsg(result.message);
+        }
+      };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await getReportData("/f3");
-      if (result.status === 200) {
-        setReportData(result.data);
-        console.log(result.data);
-      } else {
-        setErrMsg(result.message);
-      }
-    };
+      fetchData();
+    }, []);
 
-    fetchData();
-  }, []);
+    if (!reportData) {
+      return <div>{errMsg}</div>;
+    }
 
-  if (!reportData) {
-    return <div>{errMsg}</div>;
-  }
-
-  function flattenData(data: ReportResponseType) {
-    const result: any[] = [];
-    data?.data.forEach((detail) => {
-      result.push({
-        Item: detail[0],
-        "Amount (KIP)": detail[1],
+    function flattenData(data: ReportResponseType) {
+      const result: any[] = [];
+      data?.data.forEach((detail) => {
+        result.push({
+          Item: detail[0],
+          "Amount (KIP)": detail[1],
+        });
       });
-    });
 
-    return result;
-  }
-  return (<main className="flex justify-center pt-7">
-    <div className="grid place-items-center w-9/12 p-8 shadow-lg border-2 border-neutral-800">
-      <ReportHeader reportMessage="ຂໍ້ມູນຜົນການດໍາເນີນງານ" />
-      <Table className="border border-neutral-800 shadow-xl">
-        <TableHeader>
-          <TableRow className="divide-x border border-neutral-800 divide-y divide-neutral-800">
-            {reportData?.columns.map((colmunsName, index) => (
-              <TableHead key={index} className="text-center font-bold">
-                {colmunsName}
-              </TableHead>
-            ))}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {reportData?.data.map((data, index) => (
-            <TableRow
-              key={index}
-              className="border border-neutral-800 divide-x divide-neutral-800"
-            >
-              {data.map((detail, index) => (
-                <TableCell
+      return result;
+    }
+    return (
+      <main className="flex justify-center pt-7">
+        <div className="grid place-items-center w-9/12 p-8 shadow-lg border-2 border-neutral-800">
+          <ReportHeader reportMessage="ຂໍ້ມູນຜົນການດໍາເນີນງານ" />
+          <Table className="border border-neutral-800 shadow-xl">
+            <TableHeader>
+              <TableRow className="divide-x border border-neutral-800 divide-y divide-neutral-800">
+                {reportData?.columns.map((colmunsName, index) => (
+                  <TableHead key={index} className="text-center font-bold">
+                    {colmunsName}
+                  </TableHead>
+                ))}
+              </TableRow>
+            </TableHeader>
+            <TableBody>
+              {reportData?.data.map((data, index) => (
+                <TableRow
                   key={index}
-                  className={
-                    typeof detail === "number" ? "text-right" : "text-left"
-                  }
+                  className="border border-neutral-800 divide-x divide-neutral-800"
                 >
-                  {typeof detail === "number" ? formatNumber(detail) : detail}
-                </TableCell>
+                  {data.map((detail, index) => (
+                    <TableCell
+                      key={index}
+                      className={
+                        typeof detail === "number" ? "text-right" : "text-left"
+                      }
+                    >
+                      {typeof detail === "number"
+                        ? formatNumber(detail)
+                        : detail}
+                    </TableCell>
+                  ))}
+                </TableRow>
               ))}
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-      <DownloadButton
-        data={flattenData(reportData)}
-        fileName="Report_F2"
-        sheetName="Report F2"
-      />
-    </div>
-  </main>);
-}
+            </TableBody>
+          </Table>
+          <DownloadButton
+            data={flattenData(reportData)}
+            fileName="Report_F2"
+            sheetName="Report F2"
+          />
+        </div>
+      </main>
+    );
+  },
+});
