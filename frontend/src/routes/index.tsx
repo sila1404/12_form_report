0d55167bb727c3e57817d746ef5bde0aa1590ff5
sila1404 <silamany1404@gmail.com>
2024-10-23 17:19:22 +0700

    chore: if ever visit root route just redirect to report f9

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,22 +1,9 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
 
-const isImported = false;
-
 export const Route = createFileRoute("/")({
-  loader: () => {
-    if (isImported) {
-      throw redirect({
-        to: "/f2",
-      });
-    }
+  beforeLoad: () => {
+    throw redirect({
+      to: "/f9",
+    });
   },
-  component: Root
 });
-
-function Root() {
-  return (
-    <>
-      <h1 className="w-screen grid place-items-center">Import the file to see the report.</h1>
-    </>
-  )
-}
